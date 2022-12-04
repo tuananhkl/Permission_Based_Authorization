1a5b55eac1f63f9d5c717ec28b605e141688e33f
Nguyen Tuan Anh <tuananhkl99@gmail.com>
2022-12-04 17:48:56 +0700

    Displaying a List of Registered Users

---
+++ PermissionBasedAuthorizationApp/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace PermissionBasedAuthorizationApp.Controllers
+{
+    [Authorize]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+            var allUsersExceptCurrentUser = await _userManager.Users.Where(a => a.Id != currentUser.Id).ToListAsync();
+            return View(allUsersExceptCurrentUser);
+        }
+    }
+}
