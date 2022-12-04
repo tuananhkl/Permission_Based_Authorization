b95376b5c734ec34e2ec131a104591c6ab55789f
Nguyen Tuan Anh <tuananhkl99@gmail.com>
2022-12-04 18:09:48 +0700

    Role Controller + View, UserRolesViewModel class

---
+++ PermissionBasedAuthorizationApp/Models/UserRolesViewModel.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace PermissionBasedAuthorizationApp.Models
+{
+    public class ManageUserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public IList<UserRolesViewModel> UserRoles { get; set; }
+    }
+    public class UserRolesViewModel
+    {
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+    }
+}
