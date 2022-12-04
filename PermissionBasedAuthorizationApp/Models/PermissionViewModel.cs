bb71c7f06eda143e4c4fb573a974ea579fbf6ee7
Nguyen Tuan Anh <tuananhkl99@gmail.com>
2022-12-04 20:25:09 +0700

    Roles-Permission Management UI + Claims Helper

---
+++ PermissionBasedAuthorizationApp/Models/PermissionViewModel.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace PermissionBasedAuthorizationApp.Models
+{
+    public class PermissionViewModel
+    {
+        public string RoleId { get; set; }
+        public IList<RoleClaimsViewModel> RoleClaims { get; set; }
+    }
+    public class RoleClaimsViewModel
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+        public bool Selected { get; set; }
+    }
+}
