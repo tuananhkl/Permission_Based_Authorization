b21c5fcd0c0afdf00622e20668a66509f0344f13
Nguyen Tuan Anh <tuananhkl99@gmail.com>
2022-12-04 17:11:12 +0700

    Seeding Users and Roles

---
+++ PermissionBasedAuthorizationApp/Seeds/DefaultRoles.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using PermissionBasedAuthorizationApp.Constants;
+
+namespace PermissionBasedAuthorizationApp.Seeds
+{
+    public static class DefaultRoles
+    {
+        public static async Task SeedAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            await roleManager.CreateAsync(new IdentityRole(Roles.SuperAdmin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Basic.ToString()));
+        }
+    }
+}
